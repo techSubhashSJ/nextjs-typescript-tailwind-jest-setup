382593706559bcbfadc6223616035f318d816aa5
Subhash Jadhav <subhash.jadhav@bluepineapple.io>
2022-11-01 15:04:30 +0530

    jest setup added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
 import type { NextPage } from "next";
+import Head from "next/head";
+
 
 const Home: NextPage = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <h1 className="text-3xl font-bold underline" role="heading">Welcome to next.js!</h1>;
 };
 
 export default Home;
